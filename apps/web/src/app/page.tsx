b4eb8d27714fa2c2c6b286e0567ac91874b5393c
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-07 17:47:29 -0600

    Add CTA component to home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from 'next';
 
+import CTA from '../components/home/CTA';
+
 export async function generateMetadata(): Promise<Metadata> {
   return;
 }
 
 export default async function HomePage() {
-  return <></>;
+  return (
+    <>
+      <CTA />
+    </>
+  );
 }
---
+++ apps/web/src/components/home/CTA.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+import { Heading } from 'ui/src/Heading';
+import { Text } from 'ui/src/Text';
+
+import BG from '../../../public/portfolio_bg.webp';
+
+export default function CTA() {
+  return (
+    <header role="banner" className="relative h-[95vh] w-full">
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+          <Heading as="h1" size="9">
+            EDSON JARAMILLO
+          </Heading>
+          <Text as="p" size="7" textColor="gray">{`I'm a Fullstack Developer`}</Text>
+        </div>
+      </div>
+      <Image src={BG} alt="Background" fill className="z-[-1] object-cover" priority />
+    </header>
+  );
+}
