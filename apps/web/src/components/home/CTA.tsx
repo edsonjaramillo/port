e3f74537f4532c53544f0d212c4e1f2db00b7465
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-13 14:18:28 -0600

    Refactor CTA component with updated heading styles

--- apps/web/src/components/home/CTA.tsx
+++ apps/web/src/components/home/CTA.tsx
@@ -1,18 +1,23 @@
 import BlurImage from 'ui/src/BlurImage';
 import { Heading } from 'ui/src/Heading';
 import { Text } from 'ui/src/Text';
+import { cn } from 'ui/src/lib/tw';
 
 import BG from '../../../public/portfolio_bg.webp';
 
 export default function CTA() {
+  const headingCls = cn(
+    'text-4xl font-bold leading-8 tracking-8 text-grayscale-50 md:text-5xl md:leading-9 md:tracking-9',
+  );
+
   return (
     <header role="banner" className="relative h-[95vh] w-full">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-center gap-4">
-          <Heading as="h1" size="9">
-            EDSON JARAMILLO
-          </Heading>
-          <Text as="p" size="7" textColor="gray">{`I'm a Fullstack Developer`}</Text>
+          <h1 className={headingCls}>EDSON JARAMILLO</h1>
+          <Text as="p" size="7" textColor="gray">
+            Full Stack Software Engineer
+          </Text>
         </div>
       </div>
       <BlurImage src={BG} alt="Background" fill className="z-[-1] object-cover" priority />
