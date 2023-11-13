fdd32e590b5faed341e612b672010874e16f71fb
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-13 15:52:59 -0600

    Refactor sitemap function to include project links

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,25 +1,39 @@
 import { MetadataRoute } from 'next';
 
+import { CMSClient } from 'utils/src/cms/CMSClient';
 import { ENV } from 'utils/src/env';
 
 import { navigationLinks } from '../resources/links';
 
+// check to see if the href is a href with # or not
+function isHash(href: string) {
+  return /^#/.test(href);
+}
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const URL = ENV.URL;
   const now = new Date();
-  const coreLinks = navigationLinks.map((link) => ({
-    url: `${URL}${link.href}`,
+
+  const coreLinks: MetadataRoute.Sitemap = [];
+  for (const link of navigationLinks) {
+    if (isHash(link.href)) {
+      continue;
+    }
+    coreLinks.push({
+      url: `${URL}${link.href}`,
+      lastModified: now,
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    });
+  }
+
+  const projects = await new CMSClient().getProjects();
+  const projectLinks: MetadataRoute.Sitemap = projects.map((p) => ({
+    url: `${URL}/project/${p.slug}`,
     lastModified: now,
     changeFrequency: 'weekly',
     priority: 0.8,
-  })) as MetadataRoute.Sitemap;
+  }));
 
-  const otherLinks = ['donations', 'sitemap'].map((link) => ({
-    url: `${URL}/${link}`,
-    lastModified: now,
-    changeFrequency: 'weekly',
-    priority: 0.8,
-  })) as MetadataRoute.Sitemap;
-
-  return [...coreLinks, ...otherLinks];
+  return [...coreLinks, ...projectLinks];
 }
