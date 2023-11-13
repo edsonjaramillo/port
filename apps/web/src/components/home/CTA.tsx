7fd27b752e9a13df648909f86d879635dadc5f3a
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-13 16:40:10 -0600

    Refactor CTA component by removing unused imports and updating subheading.

--- apps/web/src/components/home/CTA.tsx
+++ apps/web/src/components/home/CTA.tsx
@@ -1,6 +1,4 @@
 import BlurImage from 'ui/src/BlurImage';
-import { Heading } from 'ui/src/Heading';
-import { Text } from 'ui/src/Text';
 import { cn } from 'ui/src/lib/tw';
 
 import BG from '../../../public/portfolio_bg.webp';
@@ -10,14 +8,16 @@ export default function CTA() {
     'text-4xl font-bold leading-8 tracking-8 text-grayscale-50 md:text-5xl md:leading-9 md:tracking-9',
   );
 
+  const subHeadingCls = cn(
+    'text-3xl leading-7 tracking-7 text-grayscale-neutral-light-accessible md:text-4xl md:leading-8 md:tracking-8',
+  );
+
   return (
     <header role="banner" className="relative h-[95vh] w-full">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <div className="flex h-full w-full flex-col items-center justify-center gap-4">
           <h1 className={headingCls}>EDSON JARAMILLO</h1>
-          <Text as="p" size="7" textColor="gray">
-            Full Stack Software Engineer
-          </Text>
+          <p className={subHeadingCls}>Full Stack Engineer</p>
         </div>
       </div>
       <BlurImage src={BG} alt="Background" fill className="z-[-1] object-cover" priority />
