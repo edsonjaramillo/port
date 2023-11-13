98faa2cc8765b2f6ecc4d36103ea91995b819a73
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-11-13 14:18:21 -0600

    Refactor AboutSection component layout and update About Me section content

--- apps/web/src/components/home/AboutSection.tsx
+++ apps/web/src/components/home/AboutSection.tsx
@@ -6,21 +6,26 @@ import { Text } from 'ui/src/Text';
 export default function AboutSection() {
   return (
     <Section header="About Me" headerAs="h2" id="about">
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-16 md:grid-cols-2 md:gap-0">
         <div className="relative aspect-1 overflow-hidden rounded md:m-auto md:w-96 lg:m-0 lg:w-auto">
           <AboutMeImage />
         </div>
-        <div className="flex flex-col justify-center gap-4 p-8 md:p-12">
+        <div className="flex flex-col justify-center gap-6 p-8 md:p-12">
           <Heading as="h3" size="8">
-            Getting to know me...
+            Getting to know me
           </Heading>
           <Text as="p" size="4" textColor="gray">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint consequatur delectus? Nisi, hic. Laborum
-            nulla quis, provident cumque, aliquid quo numquam non cum iste sint doloremque dolor, veniam hic consequatur
-            deserunt autem? Vitae nulla accusamus, placeat quis temporibus dolor.
+            I'm Edson Jaramillo, a dedicated and versatile Full Stack Software Engineer with a rich background in web
+            technologies. Holding a Bachelor's degree in Computer Science from the University of Alabama in Huntsville,
+            I've honed my skills to master both the front and back ends of web development. My passion for crafting
+            seamless digital experiences is the cornerstone of my freelance career. I thrive on the challenges and
+            intricacies of web development and anything software-related. Alongside my core programming expertise, I
+            also specialize in Search Engine Optimization (SEO), User Interface (UI) and User Experience (UX) design,
+            and automation scripting—ensuring a comprehensive, top-to-bottom approach to the projects I undertake. My
+            goal is to create innovative solutions that not only meet but exceed client expectations.
           </Text>
-          <div className="flex w-full justify-between">
-            <Milestone main="2021" sub="UAH Graduate" />
+          <div className="flex w-full flex-col justify-between gap-4 sm:flex-row sm:gap-0">
+            <Milestone main="2021" sub="CS Graduate" />
             <Milestone main="3.1 GPA" sub="Dean's List" />
             <Milestone main="Bilingual" sub="English & Spanish" />
           </div>
